d7b16ecc5d1047593eff94da6bd983af48e995e0
Simon Busch <simon.busch@live.fr>
2021-12-12 10:01:03 +0100

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import {ethers} from 'ethers';
-import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
 import Token from './artifacts/contracts/Token.sol/Token.json';
 
 import './App.css';
 
 
-const greeterAddress = "0x1F83E575ce789079925942F0177852EaB4F71d86";
 const tokenAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
 
 const App = () => {
-  const [greeting, setGreeting] = useState('');
   const [userAccount, setUserAccount] = useState('');
   const [amount, setAmount] = useState(0);
 
@@ -41,45 +38,9 @@ const App = () => {
     }
   }
 
-  const fetchGreeting = async () => {
-    if (typeof window.ethereum !== undefined) {
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider);
-      try {
-        const data = await contract.greet();
-        console.log("data: ", data);
-      } catch(err) {
-        console.log("error: ",err);
-      }
-    }
-  }
-
-  const setGreetingValue = async () => {
-    if (!greeting) return;
-    if (typeof window.ethereum !== undefined) {
-      await requestAccount();
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const signer = provider.getSigner();
-      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer);
-      const transaction = await contract.setGreeting(greeting);
-      setGreeting('');
-      await transaction.wait();
-      fetchGreeting();
-    }
-  }
-
-
   return (
     <div className="app">
       <header className="App-header">
-        <button onClick={fetchGreeting}>Fetch greeting</button>
-        <button onClick={setGreetingValue}>Set greeting</button>
-        <input 
-          onChange={e => setGreeting(e.target.value)}
-          placeholder='Set greetings'
-          value={greeting}
-        />
-
         <br/>
         <br/>
         <button onClick={getBalance}>Get balance</button>
